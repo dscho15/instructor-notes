d23ff2035ca371a7d9464f3e5d88d1932b83cd73
daniel <dscho15@student.sdu.dk>
2021-11-26 09:12:07 +0100

    updated lec10

--- dsp/lec10/lec10.m
+++ dsp/lec10/lec10.m
@@ -1,25 +1,22 @@
-% define vars
+% define variables
 fs = 1000
 T = 1/fs
 L = 1500
 t = (0:L-1)*T
 
-% make figure
-subplot(4, 1, 1)
-plot(t, x, 'r')
-xlabel('t [s]')
-ylabel('x(t)')
-
-% perform FFT
+% perform fft
 X = fft(x)
 X = fftshift(X)
-
-%X_shifted = fftshift(X)
 
 % double spectrum is desired
 f = fs/L*(-L/2:L/2-1)
 
-% plot it
+subplot(4, 1, 1)
+plot(t, x, 'r')
+xlabel('t [s]')
+ylabel('x(t)')
+title('Signal x(t)')
+
 subplot(4, 1, 2)
 plot(f, abs(X)/L)
 title('Single-Sided Amplitude Spectrum of x(t)')
@@ -29,7 +26,7 @@ ylabel('|X(f)|')
 X(abs(X)/L < 0.13) = 0
 subplot(4, 1, 3)
 plot(f, abs(X)/L)
-title('Single-Sided Amplitude Spectrum of x(t)')
+title('Filtered Single-Sided Amplitude Spectrum of x(t)')
 xlabel('f [Hz]')
 ylabel('|X(f)|')
 
@@ -38,9 +35,7 @@ subplot(4, 1, 4)
 plot(t, x_filtered)
 xlabel('t [s]')
 ylabel('x_{filtered}(t)')
-
-% perform ifft
-%x = ifft(P1)
+title('Filtered Signal x(t)')
 
 
 
